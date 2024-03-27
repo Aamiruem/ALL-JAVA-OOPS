168ae8a61888c20a97c6a55f31e3fe02fe3d6971
Aamir@786 <aamirhussainazad786@gmail.com>
2024-03-27 21:01:31 +0530

    commit add

--- OOPS/COMPANY/StudentClass.java
+++ OOPS/COMPANY/StudentClass.java
@@ -1,10 +1,18 @@
-package OOPS.COMPANY;
+// package OOPS.COMPANY;
 
 public class StudentClass {
     public static class Student {
-        
+        String name;
+        int rollNo;
+        double percent;
     }
     public static void main(String[] args) {
-        System.out.println("Hello Aamir");
+        Student x = new Student();
+        x.name = "Aamir";
+        x.rollNo = 1;
+        x.percent = 98.0;
+        System.out.println(x.name);
+        System.out.println(x.rollNo);
+        System.out.println(x.percent);
     }
 }
