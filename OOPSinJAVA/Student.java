2a265c52a85efa07311027db453c8658cfa8e1c6
Aamir@786 <aamirhussainazad786@gmail.com>
2024-03-29 00:29:06 +0530

    commit add

--- OOPSinJAVA/Student.java
+++ OOPSinJAVA/Student.java
@@ -1,7 +1,20 @@
-package OOPSinJAVA;
+// package OOPSinJAVA;
+
+// Access Modifiers
+// 1) Public - all packages
+// 2) Private - same class
+// 3) Default - same package
 
 public class Student {
 
-    public String name;
+    String name;
+    int rollNo;
+    double percent;
 
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.name = "Kamran";
+
+        System.out.println(s.name);
+    }
 }
