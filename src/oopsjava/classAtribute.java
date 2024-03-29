30761a1a0afc19ee9fef1bd83de782d6a6cfa9da
Aamir@786 <aamirhussainazad786@gmail.com>
2024-03-30 01:04:07 +0530

    commit add

--- src/oopsjava/classAtribute.java
+++ src/oopsjava/classAtribute.java
@@ -1,5 +1,13 @@
-// package ALL-JAVA-OOPS.src.oopsjava;
+// package oopsjava;
 
 public class classAtribute {
-    
+    String name;
+    int roll_no;
+
+    public static void main(String[] args) {
+        classAtribute obj = new classAtribute();
+        obj.name = "Aamir";
+        obj.roll_no = 1;
+        System.out.println("This my name: " + obj.name + " and Roll_No." + obj.roll_no);
+    }
 }
