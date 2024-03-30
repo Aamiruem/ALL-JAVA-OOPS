cb397e03ffc5a1b2859017feb27dab1802bc1c87
Aamir@786 <aamirhussainazad786@gmail.com>
2024-03-31 00:13:57 +0530

    commit

---
+++ src/oopsjava/Dog.java
@@ -0,0 +1,23 @@
+// package ALL-JAVA-OOPS.src.oopsjava;
+
+//java class methods
+
+public class Dog {
+    String name;
+    int license_id;
+
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+
+    public class demo {
+    }
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.name = "Rex";
+        dog.eat();
+
+    }
+}
--- src/oopsjava/Student.java
+++ src/oopsjava/Student.java
@@ -1,12 +1,24 @@
 // package ALL-JAVA-OOPS.src.oopsjava;
 
+// Java Class Attributes
+// Class Student{ Final Keyword In Java -
+// String name;
+// int Roll_no;
+// String address;
+// }
+// Main(){
+// Student std1 = new Student();
+// std1.name = "Rahul”;
+// std1.Roll_no = 11;
+// std1.address = "XYZ Colony India"
+// }
 public class Student {
     final String name = "kamran";
     int roll_No;
     String address;
 
     public class demo {
-        
+
     }
     public static void main(String[] args) {
         Student s = new Student();
