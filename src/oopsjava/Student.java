83aa44e6ed26a3d139314c7c92723aa33c7c1ac7
Aamir@786 <aamirhussainazad786@gmail.com>
2024-03-31 00:02:51 +0530

    commit add

---
+++ src/oopsjava/Student.java
@@ -0,0 +1,14 @@
+// package ALL-JAVA-OOPS.src.oopsjava;
+
+public class Student {
+    final String name = "kamran";
+    int roll_No;
+    String address;
+
+    public class demo {
+        public static void main(String[] args) {
+            Student s = new Student();
+            System.out.println(s.name);
+        }
+    }
+}
